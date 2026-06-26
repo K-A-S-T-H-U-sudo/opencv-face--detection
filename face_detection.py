3ae25fd401bc3a643baa6a468c0c48d770d0dddc
K-A-S-T-H-U-sudo <kasthurikabil2008@gmail.com>
2026-06-26 20:25:23 +0530

    Create face_detection.py

---
+++ face_detection.py
@@ -0,0 +1,19 @@
+import cv2
+alg = "haarcascade_frontalface_default.xml"
+haar_cascade = cv2.CascadeClassifier(alg)
+
+cam = cv2.VideoCapture (1) #Cam id initialization
+while True:
+
+     _,img = cam.read() #Reading the frame FRM CAM
+     grayImg = cv2.cvtColor (img, cv2. COLOR BGR2GRAY) #CONVERTING CLR IMAGE TO GRAY face = haar_cascade.detectMultiScale (grayImg, 1.3,4) #Getting coordinates
+     face=haar_cascade.detectMultiScale(grayImg,1.3,4)
+     for (x, y, w,h) in face:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0),2) 
+     cv2.imshow("FaceDetection", img)
+     key = cv2.waitKey(10)
+     print (key)
+     if key == 27:
+        break
+cam.release()
+cv2.destroyAllWindows()
